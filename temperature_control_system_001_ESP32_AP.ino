33b2cdefa862a3cf4f3ce3e064aae2e0addef522
Hanna <hanna.paivasto@gmail.com>
2025-10-04 15:57:25 +0300

    code_pre

--- temperature_control_system_001_ESP32_AP.ino
+++ temperature_control_system_001_ESP32_AP.ino
@@ -61,7 +61,7 @@ void setup() {
 
 void loop() {
   testReadData(); // for Uart testing
-  //readData(); // for Uart read all
+  //readData(); // for Uart read all temperatures
   // Listen for incoming clients
   WiFiClient client = server.available();
 
