9403ea23de6c3929b36edd73629789a74b739a2f
Hanna <hanna.paivasto@gmail.com>
2025-11-16 14:17:51 +0200

    code_pre

--- temperature_control_system_001_ESP32_AP.ino
+++ temperature_control_system_001_ESP32_AP.ino
@@ -182,5 +182,15 @@ void readData() {
         break;
       }
     }
+  switch(expression) {
+    case x:
+    // code block
+    break;
+    case y:
+    // code block
+    break;
+    default:
+    // code block
+}
   }
 }
